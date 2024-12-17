858c9ef07e271e03c4b8d93b6deb81ecd993e88a
SammyNG-dev <sammy.nguyenvanbau@gmail.com>
2024-12-17 13:49:07 +0100

    quest 3 monorepo finish

--- server/src/router.ts
+++ server/src/router.ts
@@ -8,10 +8,14 @@ const router = express.Router();
 
 // Define item-related routes
 import itemActions from "./modules/item/itemActions";
+import sayActions from "./modules/item/sayActions";
+import seriesActions from "./modules/item/seriesActions";
 
 router.get("/api/items", itemActions.browse);
 router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
+
+router.get("/api/programs", seriesActions.browse);
 
 /* ************************************************************************* */
 
